137b3a92bc6c5a473ee41b21fcde274cd9eb6b2b
Sarthak Agrawal <agarwalsarthak442@gmail.com>
2026-07-09 22:39:13 -0700

    chore: replace InitialCreate with empty InitialBaseline migration

---
+++ StockPlatform.API/Migrations/20260710044740_InitialBaseline.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlatform.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialBaseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // baseline - schema already created via 06_run_all.sql
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // baseline - nothing to revert
+        }
+    }
+}
